b3fbf5af7080c718233f6d557f5a307260b73bab
Rabbi01521 <ishan0563@gmail.com>
2021-09-09 15:02:29 +0600

    some style changes

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
 const validEmailRe =
-  /^[-a-z0-9~!$%^&*_=+}{'?]+(.[-a-z0-9~!$%^&*_=+}{'?]+)*@([a-z0-9_][-a-z0-9_]*(.[-a-z0-9_]+)*.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+  /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
 
 document.getElementById("submit-btn").addEventListener("click", () => {
   const inputValue = document.getElementById("input-field").value;
+  document.getElementById("message").innerText = "";
   //   we get the value from the input box and passing it as a parameter to test function which gives us boolan true or false depending upon the regex pattern.
   const output = validEmailRe.test(inputValue); // true or false
   if (output) {
-    console.log("Email is valid");
+    // console.log("Email is valid");
+    document.getElementById("message").innerText = "Email is valid";
+    document.getElementById("message").style.color = "green";
   } else {
-    console.log("Email is not valid");
+    document.getElementById("message").innerText = "Email is not valid";
+    document.getElementById("message").style.color = "red";
+    document.getElementById("input-field").value = "";
   }
 });
 
